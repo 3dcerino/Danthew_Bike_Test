885139f2ff602090bf9311d6a64c3738973bb41c
MJVRAR <mathewforthewin@gmail.com>
2021-04-05 23:22:27 -0700

    New Bike

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 2.5f;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //
+        MoveObject();
+        MyInput ();
+    }
+
+    void MoveObject()
+    {
+        //transform.Translate(speed * Vector3.right * Time.deltaTime);
+        transform.Translate(0, speed * Time.deltaTime, 0);
+    }
+    void MyInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            speed = 0;
+        }
+    }
+}
