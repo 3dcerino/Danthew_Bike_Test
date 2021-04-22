2173e25b8ae2cd42c4fce7276b7095145c8db624
3dcerino <62118179+3dcerino@users.noreply.github.com>
2021-04-22 13:06:45 -0500

    Added Pivots and Widgets to DANIEL scene.
    Needs headset testing.

---
+++ Assets/DANIEL/Scripts/TriggerUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerUI : MonoBehaviour
+{
+    public GameObject UiObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UiObject.SetActive(false);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            UiObject.SetActive(true);
+            Debug.Log(other.name);
+        }
+        Debug.Log("ENTER");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        UiObject.SetActive(false);
+
+        Debug.Log("EXIT");
+    }
+}
